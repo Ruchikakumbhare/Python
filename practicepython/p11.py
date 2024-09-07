ad51f696604ccb8280f681bedb07d5ce2e509015
Ruchikakumbhare <ruchikakhumbhare@gmail.com>
2024-09-08 00:01:35 +0530

    added

---
+++ practicepython/p11.py
@@ -0,0 +1,34 @@
+# l1=open('pp1.txt','w')
+# l1.write('Practice practice!!!!')
+# l1.close()
+
+
+# l1=open('pp1.txt','r')
+# x=l1.read()
+# print(x)
+# l1.close()
+
+# l1=open('pp1.txt','+a')
+# l1.write("\n  hello hello hello!!1")
+# l1.read()
+# l1.close()
+
+# x1=open('pp1.txt','w')
+# x=input('ENter the NUmber : \n')
+# print(x)
+# x1.close()
+
+x2=open('pp1.txt','w')
+print('please enter "1" to exit ')
+while(str!="1"):
+    str = input('please enter multiple name : ')
+    if str !="1":
+        x2.write(str + "\n")
+x2.close()
+# x5=open('name.txt','w')
+# print('please enter "@" to exit')
+# while(str!="@"):
+#     str = input('please enter multiple name : ')
+#     if str != "@":
+#         x5.write(str +"\n") 
+# x5.close()
